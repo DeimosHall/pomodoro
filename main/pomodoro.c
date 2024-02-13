546ffa6effde458f79119aa72511b35edf321799
deimos <deimoshall@gmail.com>
2024-02-13 16:32:31 -0600

    feat: add header

--- main/pomodoro.c
+++ main/pomodoro.c
@@ -1,3 +1,11 @@
+/*
+ * Project: Pomodoro Timer using ESP32
+ * Author: Francisco Torres (deimoshall.dev)
+ * Date: February 2024
+ * License: MIT
+ * Repository: https://github.com/DeimosHall/pomodoro.git
+ */
+
 #include <stdio.h>
 
 void app_main(void)
